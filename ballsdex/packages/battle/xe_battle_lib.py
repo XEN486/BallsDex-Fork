f95133c62b233c69e99d7a532a9873b3f838ad3d
XEN486 <markvarga11@yahoo.com>
2024-09-14 23:00:03 +0100

    battle ui improvements

--- ballsdex/packages/battle/xe_battle_lib.py
+++ ballsdex/packages/battle/xe_battle_lib.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import random
 
+
 @dataclass
 class BattleBall:
     name: str
@@ -9,47 +10,41 @@ class BattleBall:
     attack: int
     dead: bool = False
 
+
 @dataclass
 class BattleInstance:
     p1_balls: list = field(default_factory=lambda: [])
     p2_balls: list = field(default_factory=lambda: [])
     winner: str = ""
 
+
 def attack(current_ball, enemy_balls):
-    # random enemy
     enemy = random.choice(enemy_balls)
 
-    # attack with random modifier
     attack_dealt = int(current_ball.attack * random.uniform(0.5, 1.5))
     enemy.health -= attack_dealt
 
-    # check if it died
     if enemy.health <= 0:
         enemy.health = 0
         enemy.dead = True
-
-    # generate text
     if enemy.dead:
-        gen_text = f'{current_ball.owner}\'s {current_ball.name} has killed {enemy.owner}\'s {enemy.name}'
+        gen_text = f"{current_ball.owner}'s {current_ball.name} has killed {enemy.owner}'s {enemy.name}"
     else:
-        gen_text = f'{current_ball.owner}\'s {current_ball.name} has dealt {attack_dealt} damage to {enemy.owner}\'s {enemy.name}'
-
+        gen_text = f"{current_ball.owner}'s {current_ball.name} has dealt {attack_dealt} damage to {enemy.owner}'s {enemy.name}"
     return gen_text
 
+
 def gen_battle(battle: BattleInstance):
-    # run until all of the balls of another team have died
-    while any(ball for ball in battle.p1_balls if not ball.dead) and any(ball for ball in battle.p2_balls if not ball.dead):
+    while any(ball for ball in battle.p1_balls if not ball.dead) and any(
+        ball for ball in battle.p2_balls if not ball.dead
+    ):
         alive_p1_balls = [ball for ball in battle.p1_balls if not ball.dead]
         for ball in alive_p1_balls:
             yield attack(ball, battle.p2_balls)
-
         alive_p2_balls = [ball for ball in battle.p2_balls if not ball.dead]
         for ball in alive_p2_balls:
             yield attack(ball, battle.p1_balls)
-
-    # determine winner
     if all(ball.dead for ball in battle.p1_balls):
         battle.winner = battle.p2_balls[0].owner
-
     if all(ball.dead for ball in battle.p2_balls):
         battle.winner = battle.p1_balls[0].owner
